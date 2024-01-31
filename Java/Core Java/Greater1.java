f961bd4c96fde3f693f9255f13d380deed52b1e5
CodeWave Studio <95101283+CodeWaveStudio@users.noreply.github.com>
2024-01-31 23:56:14 +0530

    Add files via upload

---
+++ Java/Core Java/Greater1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Greater1{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter three number:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if(a>b&&a>c)
+System.out.println("The Greatest is" +a);
+else if(b>a&&b>c)
+System.out.println("The Greatest is"+b);
+else  if(c>a&&c>b)
+System.out.println("The Greatest is"+c);
+else
+  System.out.println("The numbers are same");
+}}
