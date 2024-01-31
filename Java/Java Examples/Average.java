dfe19ec5410a7e8bdc22ef4908b7367351c44980
CodeWave Studio <95101283+CodeWaveStudio@users.noreply.github.com>
2024-02-01 00:24:10 +0530

    Add files via upload

--- Java/Java Examples/Average.java
+++ Java/Java Examples/Average.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class Average {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int sum=0;
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter The Size Of Array:");
-	int n=sc.nextInt();
-	int []a=new int [n];
-	System.out.println("Enter The Values:");
-	for(int i=0;i<a.length;i++) {
-		a[i]=sc.nextInt();	
-		sum=sum+a[i];}
-
-			
-	System.out.println("Average Of Given Array is="+sum/n);
-	}
-
+import java.util.Scanner;
+public class Average{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter The Size Of Array:");
+int n=sc.nextInt();
+int[] a=new int[n];
+System.out.println(" The "+n+ " numbers Of Array is:");
+for(int i=0;i<n;i++){
+a[i]=sc.nextInt();
 }
+System.out.println(" The Array is:");
+for(int i=0;i<n;i++){
+System.out.println(a[i]);}
+int sum=0;int avg;
+System.out.println(" The Average Of Array is:");
+for(int i=0;i<n;i++){
+sum=sum+a[i];}
+System.out.println(sum/n);
+}}
